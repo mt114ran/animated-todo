84642615933a6023d9f8931b4ddc81d62e1c52c3
mt114ran <mt114r.an@gmail.com>
2021-11-23 12:39:23 +0900

    Add dark theme support

--- App.tsx
+++ App.tsx
@@ -1,21 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import AppContainer from './src/components/app-container'
+import Main from './src/screens/main'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppContainer>
+	<Main />
+    </AppContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
